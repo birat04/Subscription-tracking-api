5d110b20f28cd4f90b9803dcdb226dd31244a480
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-02-09 22:25:48 +0530

    setting env

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+import express from 'express';
+
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('Welcome to the subscription service');
+});
+
+app.listen(3000, () => {
+    console.log('subscription tracker api is running on http://localhost:3000');
+});
+
+export default app;
