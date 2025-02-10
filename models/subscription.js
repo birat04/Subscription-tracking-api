df851ddc7d259b208485a8f841c6e82552e33864
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-02-11 00:43:51 +0530

    added models

---
+++ models/subscription.js
@@ -0,0 +1,90 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required: [true, 'subscription name is required'],
+        trim: true,
+        minLength: 5,
+        maxLength: 50,
+    },
+    pricce :{
+        type : Number,
+        required : [true, 'subscription price is required'],
+        min : [0, 'price must be greater than 0'],
+    },
+    currency : {
+        type : String,
+        required : [true, 'currency is required'],
+        enum : ['USD', 'EUR', 'NPR', 'INR'],
+        default : 'INR',
+    },
+    frequency : {
+        type : String,
+        required : [true, 'frequency is required'],
+        enum : ['daily', 'weekly', 'monthly', 'yearly'],
+        default : 'monthly',
+    },
+    category : {
+        type : String,
+        required : [true, 'category is required'],
+        enum : ['sports', 'news', 'entertainment', 'education', 'lifestyle', 
+            'technology', 'finance', 'politics', 'health', 'travel', 'food', 'gaming', 'other'],
+    },
+    paymentMethod : {
+        type : String,
+        required : [true, 'payment method is required'],
+        enum : ['credit card', 'debit card', 'net banking', 'upi', 'wallet'],
+    },
+    status : {
+        type : String,
+        required : [true, 'status is required'],
+        enum : ['active', 'cancelled', 'completed', 'on hold', 'pending', 'failed', 'expired'],
+        default : 'active',
+    },
+    startDate : {
+        type : Date,
+        required : [true, 'start date is required'],
+        validdate : {
+            validator : (value) => value <= new Date(),
+            message : 'start date must be past date',
+        },
+    },
+    renewalDate : {
+        type : Date,
+        required : [true, 'renewal date is required'],
+        validdate : {
+            validator : function(value){
+                return value > this.startDate;
+            },
+            message : 'renewal date must be after start date',
+        },
+    },
+    user : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+        required : [true, 'user is required'],
+        index : true,
+    },
+}, {timestamps : true});
+
+subscriptionSchema.pre('save', function(next){
+    if(!this.renewalDate){
+        const renewalPeriods = {
+            daily : 1,
+            weekly : 7,
+            monthly : 30,
+            yearly : 365,
+        };
+        this.renewalDate = new Date(this.startDate);
+        this.renewalDate.setDate(this.renewalDate.getDate() + renewalPeriods[this.frequency]);
+    }
+    if(this.renewalDate < new Date()){
+        this.status = 'expired';
+    }
+    next();
+});
+
+const Subscription = mongoose.model('Subscription', subscriptionSchema);
+export default Subscription;
+           
---
+++ models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type : String,
+        required: [true, 'Please provide your name'],
+        trim: true,
+        minLength: 5,
+        macLength: 50,
+    },
+    email : {
+        type : String,
+        required: [true, 'please provide your email'],
+        trim: true,
+        unique: true,
+        lowercase: true,
+        match: [
+            /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+            'Please provide a valid email'
+        ],
+    },
+    password:{
+        type : String,
+        required: [true, 'Please provide a password'],
+        minLength : 8,
+        select: false,
+        
+    },
+} , {timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+export default User;
+
