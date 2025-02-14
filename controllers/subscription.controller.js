5c0de412e841e5279edbe9a808954038e43e4706
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-02-14 15:57:26 +0530

    added subscription controllers and routers and checked using postman

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -2,7 +2,18 @@ import Subscription from '../models/subscription.js';
 
 export const getSubscriptionById = async (req,res,next) => {
     try{
-        
+        const subscription = await Subscription.findById(req.params.id);
+        if(!subscription){
+            const error = new Error('Subscription not found');
+            error.statusCode = 404;
+            throw error;
+        }
+        if(String(subscription.user) !== String(req.user._id)) {
+            const error = new Error('you are not authorized to view this subscription');
+            error.statusCode = 403;
+            throw error;
+        }
+        res.status(200).json({ success: true, date: subscription});
     } catch(e){
         next(e);
     }
@@ -35,3 +46,94 @@ export const getUserSubscriptions = async (req, res, next) => {
         next(e);
     }
 };
+
+export const updateSubscriptions = async (req,res,next) => {
+    try{
+        let subscription = await Subscription.findById(req.params.id);
+        if(!subscription){
+            const error = new Error('Subscription not found');
+            error.statusCode = 404;
+            throw error;
+        }
+        if(String(subscription.user) !== String(req.user._id)){
+            const error = new Error('You are not allowed to authorized this subscription');
+            error.statusCode = 403;
+            throw error;
+        }
+        subscription = await Subscription.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new:true, runValidators: true}
+        );
+        res.status(200).json({ success: true, date: subscription});
+    } catch(e) {
+        next(e);
+    }
+}
+
+export const cancelSubscriptions = async (req,res,next) =>{
+    try{
+        let subscription = await Subscription.findById(req.params.id);
+        if(!subscription){
+            const error = new Error('Subscription not found');
+            error.statusCode = 404;
+            throw error;
+        }
+        if(String(subscription.user) !== String(req.user._id)){
+            const error = new Error('You are not authorized to cancel this subscription');
+            error.statusCode = 403;
+            throw error;
+        }
+        subscription.status = 'canceled';
+        await subscription.save();
+
+        res.status(200).json({ success: true, message: 'Subscription canceled successfully' });
+
+
+    } catch(e){
+        next(e);
+    }
+}
+
+export const deleteSubscription = async (req, res, next) => {
+    try {
+        const subscription = await Subscription.findById(req.params.id);
+
+        if (!subscription) {
+            const error = new Error('Subscription not found');
+            error.statusCode = 404;
+            throw error;
+        }
+        if (String(subscription.user) !== String(req.user._id)) {
+            error.statusCode = 403;
+            throw error;
+        }
+        await subscription.deleteOne();
+        res.status(200).json({ success: true, message: 'Subscription deleted successfully' });
+    } catch (e) {
+        next(e);
+    }
+};
+
+export const getUpcomingRenewals = async (req, res, next) => {
+    try {
+        const today = new Date();
+        const upcomingRenewals = await Subscription.find({
+            user: req.user._id,
+            nextBillingDate: { $gte: today },
+        }).sort({ nextBillingDate: 1 });
+
+        res.status(200).json({ success: true, data: upcomingRenewals });
+    } catch (e) {
+        next(e);
+    }
+};
+
+
+
+
+
+
+
+
+
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,30 +1,20 @@
 import { Router } from "express";
 import authorize from "../middleware/auth.middleware.js";
-import { createSubscription, getUserSubscriptions } from "../controllers/subscription.controller.js";
+import { createSubscription, getUserSubscriptions, getSubscriptionById, updateSubscriptions, cancelSubscriptions, deleteSubscription, getUpcomingRenewals } from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
 subscriptionRouter.get('/', (req,res) => {
     res.send('Fetch all subscription');
 });
-subscriptionRouter.get('/:id', (req,res) => {
-    res.send('Fetch a subscription by id');
-});
+subscriptionRouter.get('/user/:id',authorize,getSubscriptionById);
 subscriptionRouter.post('/', authorize, createSubscription);
 
-subscriptionRouter.put('/:id', (req,res) => {
-    res.send('Update a subscription by id');
-});
-subscriptionRouter.delete('/:id', (req,res) => {
-    res.send('Delete a subscription by id');
-});
+subscriptionRouter.put('/:id',authorize,updateSubscriptions);
+subscriptionRouter.delete('/:id',authorize, deleteSubscription);
 subscriptionRouter.get('/user/:id', authorize,getUserSubscriptions);
 
-subscriptionRouter.put('/:id/cancel', (req,res) => {
-    res.send('Cancel a subscription by id');
-});
-subscriptionRouter.get('/upcoming-renewals', (req,res) => {
-    res.send('Fetch all upcoming renewals');
-});
+subscriptionRouter.put('/:id/cancel',authorize,cancelSubscriptions);
 
+subscriptionRouter.get('/upcoming-renewals',authorize,getUpcomingRenewals )
 export default subscriptionRouter;
